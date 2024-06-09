fe509c566126ac63f41945629c4d3af8d25fe543
ManojTarakaRamaraoGunuru <manoj.gunuru@gmail.com>
2024-06-09 13:48:30 -0400

    shared memory communication extablished along with the support of named pipes

--- ipc-code/my_shm_posix_cons.c
+++ ipc-code/my_shm_posix_cons.c
@@ -1,3 +1,7 @@
+// You can find the shared memory file at /dev/shm/MY_SHARED_MEMORY
+// shm_unlink will delete that file, for validating what producer write before consumer 
+// consumes, you can use this path
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -5,29 +9,47 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define SHM_SIZE 4096
 #define SHM_NAME "MY_SHARED_MEMORY"
 
-// You can find the shared memory file at /dev/shm/MY_SHARED_MEMORY
-// shm_unlink will delete that file, for validating what producer write before consumer 
-// consumes, you can use this path
+const char* free_str = "Freeeee\0";
+const int offset = sizeof(free_str);
 
 int main(){
 
-	int shm_fd = shm_open(SHM_NAME, O_CREAT| O_RDONLY, 0666);
+	int shm_fd = shm_open(SHM_NAME, O_CREAT| O_RDWR, 0666);
 
-	void*ptr = mmap(0,SHM_SIZE,PROT_READ, MAP_SHARED, shm_fd, 0);
+	void*ptr = mmap(NULL,SHM_SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
 	if(ptr == (void*)-1){
-		printf("Error in reaching shared memory");
+		perror("mmap creation failed");
 		return 1;
 	}
+	if(mkfifo("my_fifo",0666) == -1){
+        if(errno != EEXIST){
+            perror("unable to create fifo");
+            return 1;
+        }
+    }
+	
+	int fifo_fd = open("my_fifo", O_RDONLY);
 
-	printf("%s", (char*)ptr);
+	int itr;
+	for(int cnt = 0; cnt<1000; cnt++){
+		if(read(fifo_fd, &itr, sizeof(itr)) == -1){
+			perror("unable to read");
+			return 1;
+		}
+		printf("%s - %d\n",ptr+(itr*offset), itr);
+		sprintf(ptr+(itr * offset), "%s", free_str);
+		usleep(100000);
+	}
+	close(fifo_fd);
 
 	if(shm_unlink(SHM_NAME) == -1){
-		printf("problem in closing shared memory");
+		perror("unable to delete shared memory");
 		return 1;
 	}
 	return 0;
--- ipc-code/my_shm_posix_prod.c
+++ ipc-code/my_shm_posix_prod.c
@@ -1,3 +1,7 @@
+// Goal: write "OSisFun\n" 8 bytes string 1000 times so that consumer can consume the string in 4KB shared memory
+// producer can write 512 copies of the string in the shared memory at one go, it needs to reuse the memory to rewrite the remaining
+// For that, there needs to be another way of communication of processes that is achieved through named pipes or fifo
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,33 +10,63 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <errno.h>
 
 #define SHM_SIZE 4096
 #define SHM_NAME "MY_SHARED_MEMORY"
 
-// Goal: write "OSisFun\n" 8 bytes string 1000 times so that consumer can consume the string in 4KB shared memory
-// producer can write 512 copies of the string in the shared memory at one go, it needs to reuse the memory to rewrite the remaining
-// For that, there needs to be another way of communication of processes that is achieved through named pipes or fifo
+const char* str = "OSisFun\0";
+const char* free_str = "Freeeee\0";
+const int offset = sizeof(free_str);
+
 
 int main(){
-    const char* str = "OSisFun\n";
+    
 
     // open shared memory with read and write mode with 666 permission
     int shm_fd = shm_open(SHM_NAME,O_CREAT | O_RDWR, 0666);
     // configure the size of the memory
     ftruncate(shm_fd, SHM_SIZE);
 
-    void*ptr = mmap(0, SHM_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shm_fd,0);
-
+    void*ptr = mmap(NULL, SHM_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shm_fd,0);
     if(ptr == (void *)-1){
-        printf("Creation of memory mapping failed");
+        perror("mmap creation failed");
         return 1;
     }
 
-    sprintf(ptr,"%s",str);
-    ptr += sizeof(str);
-    sprintf(ptr,"%s",str);
-    ptr += sizeof(str);    
+    if(mkfifo("my_fifo",0666) == -1){
+        if(errno != EEXIST){
+            perror("unable to create fifo");
+            return 1;
+        }
+    }
+    for(int i = 0; i<512; i++){
+        sprintf(ptr + (i*offset),"%s",free_str);
+    }
+
+    int fifo_fd = open("my_fifo",O_WRONLY); 
+
+    int itr = 0;
+    for(int cnt = 0; cnt<1000; cnt++){
+        if(cnt == 512){
+            itr = 0;
+        }
+        while(strcmp(ptr+(itr*offset),free_str) != 0){};
+
+        if(strcmp(ptr+(itr*offset),free_str) == 0){
+            sprintf(ptr + itr*offset, "%s", str);
+            printf("%s - %d\n", ptr+(itr*offset), cnt+1);
+            if(write(fifo_fd,&itr,sizeof(itr)) == -1){
+                printf("write to fifo failed");
+                return 1;
+            }
+        }
+        itr++;
+    }
+
+    close(fifo_fd);
+
+    
 
     return 0;
 }
