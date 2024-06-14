1a5b3133a00ea7c83fdebd0d793eec63007ba52a
ManojTarakaRamaraoGunuru <manoj.gunuru@gmail.com>
2024-06-14 13:41:31 -0400

    basic structured added

---
+++ malloc-code/alloc.c
@@ -0,0 +1,19 @@
+#include "alloc.h"
+
+
+int init_alloc(){
+    return 0;
+}
+int cleanup(){
+    return 0;
+}
+char *alloc(int){
+    return NULL;
+}
+void dealloc(char *){
+    
+}
+
+int main(){
+    return 0;
+}
